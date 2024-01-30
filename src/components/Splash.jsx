7973af22c58b5780302a49b130c6b423d8bf9806
kinderekson <pawlas2006@gmail.com>
2024-01-30 08:43:41 +0100

    dodalem napis na glownej i troche wyzej jest

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import "./Splash.css"
-import zdjencie from "../assets/tlo.webp"
+import React from "react";
+import "./Splash.css";
+import zdjencie from "../assets/tlo.webp";
 
 const Splash = () => {
-  return (
-    <div className='bg' style={{
-        backgroundImage:`url(${zdjencie})`
-    }} >
-        <div className='napis'>
-            SZYBKIEFURY.PL
+    return (
+        <div
+            className="bg"
+            style={{
+                backgroundImage: `url(${zdjencie})`,
+            }}
+        >
+            <div className="napisy">
+                <div className="napis2">Twoja wypożyczalnia aut</div>
+                <div className="napis">SZYBKIEFURY.PL</div>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Splash
+export default Splash;
