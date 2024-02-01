750a4de957dd6f8321379bc54255bc9afeb2c1c9
kinderekson <pawlas2006@gmail.com>
2024-02-01 20:58:24 +0100

    showcase

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import "../components/Showcase.css"
+import FERARI from "../assets/ferari.png";
 
 const Showcase = () => {
   return (
     <div className='calosc'>
         <div className='lewoduze'>
-            <div className='auto'>
-                autko
+            <div className='auto' style={{
+                backgroundImage: `url(${FERARI})`,
+            }}>
+            <div className='model'>Ferrari 458 Italia</div>
             </div>
         </div>
         <div className='prawoduze'>
