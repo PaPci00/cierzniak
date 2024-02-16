3483296d3ff4baa6e31e98867d0eb7d3d74be871
PaPci00 <99023795+PaPci00@users.noreply.github.com>
2024-02-16 08:05:23 +0100

    dashboard

---
+++ src/components/Panel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Panel.css";
+
+const Panel = () => {
+  return (
+    <>
+    <div className='witaj'>Użytkowniku! Witaj na dashboardzie SzybkieFury.pl (możesz już się wylogować)</div>
+    </>
+  )
+}
+
+export default Panel
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
 import Footer from '../components/Footer'
+import Panel from "../components/Panel"
 
 const Dashboard = () => {
   return (
     <>
         <Navbar/>
-          witaj na dashboardzie
+        <Panel />
         <Footer />
     </>
   )
